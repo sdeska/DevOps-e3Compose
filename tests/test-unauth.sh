c0c17249cd62e1db1097bcd001cb4813082a9500
Esa Karjalainen <77407605+sdeska@users.noreply.github.com>
2025-01-06 13:52:10 +0200

    Remove q flag from curl

--- tests/test-unauth.sh
+++ tests/test-unauth.sh
@@ -4,5 +4,5 @@ docker compose up -d
 sleep 5
 EXPECTED="401 Authorization Required"
 ACTUAL=$(curl http://localhost:8198/request)
-echo "$ACTUAL" | grep -q "$EXPECTED"
+echo "$ACTUAL" | grep "$EXPECTED"
 echo "Unauth test successful"
