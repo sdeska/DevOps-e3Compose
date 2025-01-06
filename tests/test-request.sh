e68c271af753ceb1e4dc9982b8c31174ad1f38e9
Esa Karjalainen <77407605+sdeska@users.noreply.github.com>
2025-01-06 13:44:05 +0200

    Sleep after curl in /request test

--- tests/test-request.sh
+++ tests/test-request.sh
@@ -4,12 +4,13 @@ EXPECTED=("Service1:" "Service2:" "Filesystem" "PID" "TTY" "STAT" "COMMAND")
 # login-curl.txt contains the credentials in the proper format for curl. Using a file for credentials like this
 # is best practice for security. Obviously the file would not be in version control in an actual case.
 RESPONSE=$(curl --netrc-file login-curl.txt http://localhost:8198/request)
+sleep 2
 echo "$RESPONSE"
 for STR in "${EXPECTED[@]}"; do
-if [[ ! "$RESPONSE" =~ "$STR" ]]; then
-    echo "Failed: Response missing expected string '$STR'"
-    docker compose down
-    exit 1
-fi
+    if [[ ! "$RESPONSE" =~ "$STR" ]]; then
+        echo "Failed: Response missing expected string '$STR'"
+        docker compose down
+        exit 1
+    fi
 done
 echo "Request test successful"
