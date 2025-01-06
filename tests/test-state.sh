e00d6cdd550863ac330bf7fa479af29f39a0f957
Esa Karjalainen <77407605+sdeska@users.noreply.github.com>
2025-01-06 17:55:10 +0200

    Implement test for changing and getting system state

---
+++ tests/test-state.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Test state transitions.
+
+# State is initially INIT.
+curl http://localhost:8197/state | grep "INIT"
+
+# Login.
+curl http://localhost:8197/state --netrc-file ./tests/login-curl.txt -X PUT -d "RUNNING" | grep "OK"
+curl http://localhost:8197/state | grep "RUNNING"
+
+# Change state to PAUSED.
+curl http://localhost:8197/state -X PUT -d "PAUSED" | grep "OK"
+curl http://localhost:8197/state | grep "PAUSED"
+
+# Nothing should happen when putting the current status.
+curl http://localhost:8197/state -X PUT -d "PAUSED" | grep "OK"
+curl http://localhost:8197/state | grep "PAUSED"
+
+# Back to INIT.
+curl http://localhost:8197/state -X PUT -d "INIT" | grep "OK"
+curl http://localhost:8197/state | grep "INIT"
+
+# Disallowed operation before login.
+curl http://localhost:8197/state -X PUT -d "PAUSED" | grep "ERROR"
+
+# Unauth attempt to change to RUNNING.
+curl http://localhost:8197/state -X PUT -d "RUNNING" | grep "ERROR"
+
+# Login again.
+curl http://localhost:8197/state --netrc-file ./tests/login-curl.txt -X PUT -d "RUNNING" | grep "OK"
+curl http://localhost:8197/state | grep "RUNNING"
+
+# Shutdown.
+curl http://localhost:8197/state -X PUT -d "SHUTDOWN" | grep "OK"
