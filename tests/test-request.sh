da7ab31065f414344519fbf7319556dae2a6322f
Esa Karjalainen <77407605+sdeska@users.noreply.github.com>
2025-01-06 13:38:52 +0200

    Echo response in /request test

--- tests/test-request.sh
+++ tests/test-request.sh
@@ -4,6 +4,7 @@ EXPECTED=("Service1:" "Service2:" "Filesystem" "PID" "TTY" "STAT" "COMMAND")
 # login-curl.txt contains the credentials in the proper format for curl. Using a file for credentials like this
 # is best practice for security. Obviously the file would not be in version control in an actual case.
 RESPONSE=$(curl --netrc-file login-curl.txt http://localhost:8198/request)
+echo "$RESPONSE"
 for STR in "${EXPECTED[@]}"; do
 if [[ ! "$RESPONSE" =~ "$STR" ]]; then
     echo "Failed: Response missing expected string '$STR'"
