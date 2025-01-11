9fe528c91e21eb01cc086dd346c7c4bd4ff683e9
Esa Karjalainen <77407605+sdeska@users.noreply.github.com>
2025-01-11 21:14:04 +0200

    mv stop_listener controller

--- tests/test-stop.sh
+++ tests/test-stop.sh
@@ -4,7 +4,7 @@
 # due to the server suddenly stopping, which is what we want in this case.
 curl -X POST --netrc-file ./tests/login-curl.txt http://localhost:8198/stop || true
 sleep 2
-UNEXPECTED=("devops-project-esa-nginx" "devops-project-esa-service1" "devops-project-esa-service2" "devops-project-esa-stop_listener")
+UNEXPECTED=("devops-project-esa-nginx" "devops-project-esa-service1" "devops-project-esa-service2" "devops-project-esa-controller")
 ACTUAL=$(docker container ls)
 for STR in "${UNEXPECTED[@]}"; do
     if [[ "$ACTUAL" =~ "$STR" ]]; then
